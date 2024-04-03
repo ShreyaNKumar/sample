9905ab3fcce9e7a3689cec5b869723f0826b01b3
ShreyaNKumar <126964046+ShreyaNKumar@users.noreply.github.com>
2024-04-03 19:52:03 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,15 +1,21 @@
-class FibonacciExample1{  
-public static void main(String args[])  
-{    
- int n1=0,n2=1,n3,i,count=10;    
- System.out.print(n1+" "+n2);//printing 0 and 1    
-    
- for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
- {    
-  n3=n1+n2;    
-  System.out.print(" "+n3);    
-  n1=n2;    
-  n2=n3;    
- }    
-  
-}}  
+pipeline {
+         agent any
+         stages {
+                 stage('One') {
+                 steps {
+                     echo 'Hi, welcome to pipeline demo...'
+                 }
+                 }
+                 stage('Two') {
+                 steps {
+                    echo('Sample testing of Stage 2')
+                 }
+                 }
+                 stage('Three') {
+                
+                 steps {
+                       echo 'Thanks for using Jenkins Pipeline'
+                 }
+                 }
+              }
+}
